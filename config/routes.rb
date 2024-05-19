b2ad4960957e3ee2c47ce48f322ee56d699b897e
himatoko <160095919+himatoko@users.noreply.github.com>
2024-05-19 04:02:51 +0000

    [Add] 管理者機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: "homes#top"
-  get '/homes/about' => 'homes#about', as: 'about'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    resource :favorite, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+  devise_for :admin, skip: [:registrations, :password], controllers: {
+    sessions: 'admin/sessions'
+  }
+  
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+    resources :users, only: [:destroy]
   end
-  resources :users, only: [:show, :edit, :update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  scope module: :public do
+    devise_for :users
+    root to: 'homes#top'
+    get 'homes/about', to: 'homes#about', as: :about
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+      resources :post_comments, only: [:create, :destroy]
+    end
+    resources :users, only: [:show, :edit, :update]
+  end
 end
